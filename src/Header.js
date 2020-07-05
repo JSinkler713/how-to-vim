f8b3a859521d7706b608280fbbd0ccf837303063
James Sinkler <jamessinkler713@gmail.com>
2020-07-04 18:09:20 -0700

    adds Header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className='Header'>
+           <h1>How to Vim</h1>
+        </div>
+    );
+}
+
+export default Header;
