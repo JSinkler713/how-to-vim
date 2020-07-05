abc935e67332f847b20b25bda36220a483615a60
James Sinkler <jamessinkler713@gmail.com>
2020-07-04 18:08:34 -0700

    uses react-player for video

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Helmet } from 'react-helmet';
+import ReactPlayer from 'react-player';
+import Header from './Header'
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Helmet>
         <title>How to Vim</title>
         <meta name='description' content='A site focused on helping developers use the fundamentals of vim and vim shortcuts' />
       </Helmet>
-      <p>Welcome to 'How to Vim'.</p>
+      <Header />
+      <ReactPlayer className='reactPlayer' playing='true' loop='true' url='vim-examples/vimExample.mov' />
     </div>
   );
 }
