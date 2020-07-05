537dd0a87ededf59c1efacefdbbbd3ed76491b96
James Sinkler <jamessinkler713@gmail.com>
2020-07-04 18:09:01 -0700

    adds example vim commands video

---
+++ public/vim-examples/edit_in_a.js
@@ -0,0 +1,13 @@
+// Here's an example of how you could edit everything inside the ()
+
+// We have a function with 1 param, which we want to edit
+
+const sumTwoNums = (num1, num2) => {
+  return num1 + num2
+}
+
+// to edit what's in the parenthesis we can use the following commands
+//  5gg  this will bring us to line 5
+//  f(  this will get us to opening parenthesis
+//  ci(  this will cut what is within the parenthesis() and put us in insert mode to replace with what we want
+
