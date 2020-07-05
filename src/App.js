6cf4e300ad2f04fe95c2f7e2e6179d1aac4acdc2
James Sinkler <jamessinkler713@gmail.com>
2020-07-04 18:55:12 -0700

    adds react-spring animation for reasons to use vim

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Helmet } from 'react-helmet';
 import ReactPlayer from 'react-player';
 import Header from './Header'
+import OpeningDescription from './OpeningDescription';
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
         <meta name='description' content='A site focused on helping developers use the fundamentals of vim and vim shortcuts' />
       </Helmet>
       <Header />
-      <ReactPlayer className='reactPlayer' playing='true' loop='true' url='vim-examples/vimExample.mov' />
+      <div className='vidContainer'>
+        <ReactPlayer className='reactPlayer' playing='true' loop='true' url='vim-examples/vimExample.mov' />
+      </div>
+      <OpeningDescription />
     </div>
   );
 }
---
+++ src/OpeningDescription.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { useTrail, animated } from 'react-spring';
+
+
+function OpeningDescription(props) {
+    const [start, setStart] = useState(0)
+    const reasons = ['no mouse', 'faster', 'stay in dev flow', 'easy', 'yes it can still be used in vscode...']
+    const config = { mass: 50, tension: 500, friction: 100, clamp: true}
+
+    useEffect (()=> {
+        setStart(true)
+    }, [])
+    
+    let trail = useTrail(reasons.length, {
+        delay: 3000,
+        from: {
+          backgroundColor: "#f1f1f1",
+          opacity: 0,
+          transform: "translate3d(0px, -80px, 0)"
+        },  
+        to: {
+          backgroundColor: "black", 
+          opacity: 1,
+          transform: "translate3d( 0, 0, 0)"
+        },  
+        config: { mass:3,  tension: 300, friction: 40, clamp: false }
+      });
+
+      const values = trail.map((props, index) => {
+        return(
+          <animated.div key={index} style={props} className='box'>
+            <p className='reason'>Why use vim: {reasons[index]}</p>
+             <br />
+          </animated.div>
+        );
+      });
+
+    return (
+        <div>
+            {start ? values : 'no bueno'}
+        </div>
+    );
+}
+
+export default OpeningDescription;
